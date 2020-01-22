5cfb4ed321efd814f6453c35b0328b0e1fe13350
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-01-22 13:54:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 5, "M", "brown", "happy", 1)
+("Rowdy", 5, "M", "brown", "happy", 1)
+("Tabitha", 5, "M", "brown", "happy", 1)
+("Sergeant Brown", 5, "M", "brown", "happy", 1)
+("Melissa", 5, "M", "brown", "happy", 1)
+("Grinch", 5, "M", "brown", "happy", 1)
+("Wendy", 5, "M", "brown", "happy", 1)
+(NULL, 5, "M", "brown", "happy", 1)
